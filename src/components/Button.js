8d34702ae9f9a6871ca30e7847bf0d63dbe5d8a2
wowgod9504 <mike1124@naver.com>
2020-08-29 21:45:54 +0900

    🎉 Practice react.js-scss making button

---
+++ src/components/Button.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Button.scss";
+import classNames from "classnames";
+
+// size : large, medium, small
+// color : blue, pink, gray
+function Button({
+  children,
+  size,
+  color,
+  outline,
+  fullWidth,
+  className,
+  ...rest
+}) {
+  console.log(rest);
+  return (
+    <button
+      className={classNames(
+        "Button",
+        size,
+        color,
+        {
+          outline,
+          fullWidth,
+        },
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  size: "medium",
+  color: "blue",
+};
+
+export default Button;
